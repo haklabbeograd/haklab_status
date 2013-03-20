77dd01d483667e2aa66b6a2af54ac679b23cac88
Vladan Popovic <vladanovic@gmail.com>
2013-03-20 21:06:06 +0100

    send DM to admins on fail

--- hklbgd_twitt.py
+++ hklbgd_twitt.py
@@ -6,11 +6,13 @@ This way more devices could be attached to the arduino and different
 clients could read data from the devices connected by querying the arduino.
 
 """
+import datetime
 import serial
 import sys
 import threading
 import twitter
-from auth import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+from auth import (CONSUMER_KEY, CONSUMER_SECRET,
+        ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 
 
@@ -18,43 +20,64 @@ SERIAL_PORT = "/dev/ttyUSB0"
 TIMEOUT = 600
 OPEN_MSG = "Hklbgd je otkljucan."
 CLOSE_MSG = "Hklbgd je zakljucan."
+ADMINS = []
 
+API = twitter.Api(consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        access_token_key=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET)
 
 
 def post_status(message):
-    api = twitter.Api(consumer_key=CONSUMER_KEY,
-            consumer_secret=CONSUMER_SECRET,
-            access_token_key=ACCESS_TOKEN,
-            access_token_secret=ACCESS_TOKEN_SECRET)
     try:
-        api.PostUpdate(message)
-        sys.stdout.write("tweet sent - %s " % message)
+        message = "%s %s" % (datetime.datetime.now(), message)
+        print(message)
+        API.PostUpdate(message)
+        print("tweet sent - %s " % message)
     except Exception as e:
-        sys.stderr.write(str(e))
+        print(str(e))
+
+
+
+def send_direct_messages(message):
+    try:
+        for user in ADMINS:
+            API.PostDirectMessage(user, message)
+        print("DM sent - %s " % message)
+    except Exception as e:
+        print(str(e))
 
 
 
 if __name__ == "__main__":
     timer = None
+    counter = 0 # in case of > 3 times fail, send DM to admins
 
-    srl = serial.Serial(SERIAL_PORT, 9600, timeout=10)
+    srl = serial.Serial(SERIAL_PORT, 9600)
 
     while True:
         try:
             # read from serial - receive "1" when unlocked, "0" when locked
             unlocked = bool(int(srl.readline()))
+            print("read from serial | unlocked=%s" % unlocked)
+            counter = 0
         except Exception as e:
-            sys.stderr.write(str(e))
-            continue
+            if counter > 3:
+                send_direct_messages("FAIL!!! - %s" % e)
+                break
+            else:
+                print(str(e))
+                counter += 1
+                continue
 
         msg = OPEN_MSG if unlocked else CLOSE_MSG
 
         if timer is not None and timer.is_alive():
             # if someone changes states 2 times isinde the TIMEOUT
-            # then cancel tweeting
+            # then cancel tweetpsurd', ' mishkez'ing
             timer.cancel()
-            sys.stdout.write("timeout not elapsed - TWITTER ABORT")
+            print("timeout not elapsed - TWITTER ABORT")
         else:
-            timer = threading.Timer(TIMEOUT, post_status, args=(msg))
+            timer = threading.Timer(TIMEOUT, post_status, args=(msg,))
             timer.start() # start timing and tweet when TIMEOUT expires
-            sys.stdout.write("timer started, tweeting in %d seconds" % TIMEOUT)
+            print("timer started, tweeting in %d seconds" % TIMEOUT)
