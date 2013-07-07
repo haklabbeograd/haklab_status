b0c339153ffede77f6b628b40a56613680857bde
Vladan Popovic <vladanovic@gmail.com>
2013-07-07 21:52:37 +0200

    remove arp client count script
    it's just that it should be done better

--- updatestatus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-cat /proc/net/arp < /dev/zero | grep br-lan | wc -l | awk '{print $1 = $1 - 1}' > /www/.number.txt
-mv /www/.number.txt /www/number.txt                                                
-
-curl --request PUT -g --header "X-ApiKey: umGTpLuz5bphaATCoACWKMc5AgQqgCGvs2OZzXw73FvugTL5" \
---data '{"version": "1.0.0", "datastreams": [{"id": "devices", "current_value": '`cat /www/number.txt`'}]}' \
-http://api.xively.com/v2/feeds/127929
